ba5d9692ced4f2b4f60f18ea353f701dab4dee7d
Jörgen Sigvardsson <jorgen.sigvardsson@gmail.com>
2021-07-22 10:25:03 +0200

    Begun toying with async I/O

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
-
-	t "github.com/jorgensigvardsson/gomud/absmachine"
+	"net"
 )
 
 func main() {
-	foo := t.Room{Description: "Hello"}
-	fmt.Printf("%+v", foo)
+	listener, err := net.Listen("tcp", ":5000")
+
+	if err != nil {
+		panic("Failed to open TCP port 5000")
+	}
+
+	for {
+		go handleConnections(listener)
+	}
+}
+
+func handleConnections(listener net.Listener) error {
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("TCP accepted", conn)
+	}
+	return nil
 }
